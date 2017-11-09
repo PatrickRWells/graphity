e06334ffb62f6164e4b6b5aa49ddfd6decc16954
PatrickRWells <32802366+PatrickRWells@users.noreply.github.com>
2017-11-09 13:30:18 -0800

    initial

---
+++ hamiltonian/hamiltonians.h
@@ -0,0 +1,17 @@
+//
+//  absHamiltonian.h
+//  
+//
+//  Created by Patrick on 10/31/17.
+//
+//
+
+
+#ifndef hamiltonians_h
+#define hamiltonians_h
+
+#include "basic.h"
+
+
+
+#endif /* hamiltonians_h */
