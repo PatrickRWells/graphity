74adb4fa4edfef5612d67a739f0d091d6e3f2c5b
PatrickRWells <32802366+PatrickRWells@users.noreply.github.com>
2017-11-19 13:11:54 -0800

    Simulate built
    Simulation should now be able to run correctly for any hamiltonian provided it is given appropriate input.

--- Simulate/Simulate.cpp
+++ Simulate/Simulate.cpp
@@ -17,14 +17,60 @@ int main() {
     int numRead;
     std::function<void(hGraph&)> simFunction;
     
-	simFunction = basicHam;
+	simFunction = basicSquareHam;
     
-    
+    hList * first;
+    hList * second;
+    hList *t;
+    first = new hList;
+    second = new hList;
     graphData = readGraphFile(numRead);
-    std::cout << graphData[0] << std::endl;
-    simFunction(graphData[0]);
-    std::cout << graphData[0] << std::endl;
+    
+    std::cout << "Calculating hamiltonians..." << std::endl;
+    
+    for(int i = 0; i < numRead; i++) {
+        simFunction(graphData[i]);
+        
+        
+        if((graphData[i].getHam() < first->getEnergy()) || first->isEmpty()) {
+            t = second;
+            second = first;
+            first = t;
+            first->clear();
+            first->prepend(graphData + i);
+            
+        }
+        
+        else if(graphData[i].getHam() == first->getEnergy()) {
+            first->prepend(graphData + i);
 
+        }
+
+        
+        else if((graphData[i].getHam() < second->getEnergy()) || second->isEmpty()) {
+            second->clear();
+            second->prepend(graphData + i);
+
+        }
+        
+        
+        else if(graphData[i].getHam() == second->getEnergy()){
+            second->prepend(graphData + i);
+
+        }
+        
+        
+    }
+    std::cout << "Output file will include lowest energy graphs, follow by those in the first excited state." <<std::endl;
+    std::cout << "Please enter an output file name: ";
+    std::string outFile;
+    std::cin >> outFile;
+    outFile += ".csv";
+    std::ofstream ofile;
+    ofile.open(outFile);
+    ofile << *first;
+    ofile << *second;
+    ofile.close();
     
     
 }
---
+++ hamiltonian/template.h
@@ -0,0 +1,51 @@
+//
+//  template.h
+//
+//
+//  Created by Patrick on 11/17/17.
+//
+//  To use this template, start by changing all instances of "Template" to the name of your file (with correct capitalization)
+
+#ifndef template_h
+#define template_h
+
+#include "absHamiltonian.h"
+#include <cmath>
+
+void TemplateHam(hGraph &host);
+class Template : public absHamiltonian {
+    
+private:
+    int _result; //Result of the computation.
+public:
+    Template();
+    double result();
+    void calculate(hGraph &host); //Don't mess with these
+    
+    
+};
+
+Template::Template() : _result(0.0) { //unlikely that it should be changed, unless you have some background energy level
+    
+}
+
+double Template::result() { //There should be no reason to edit this function;
+    return _result;
+}
+
+void Template::calculate(hGraph &host) { //This is where all the main calculation takes place.
+    int size = host.getSize();           //see hGraph documentation for available commands.
+    for(int i = 0; i < size; i++) {
+        _result += host.getDegree(i);
+    }
+    
+}
+
+void TemplateHam(hGraph &host) { //do not edit this function except to change instances of the word "Template"
+    Template Ham;
+    host.accept(Ham);
+    host.setHamiltonian(Ham.result());
+    
+}
+
+#endif /* basic2_h */
