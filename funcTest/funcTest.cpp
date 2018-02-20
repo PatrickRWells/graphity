bddb7b383e62eeff692662632192b4b9bf804189
PatrickRWells <32802366+PatrickRWells@users.noreply.github.com>
2018-02-19 19:20:57 -0800

    Incremental changes

--- funcTest/funcTest.cpp
+++ funcTest/funcTest.cpp
@@ -14,12 +14,34 @@ using namespace std;
 
 
 int main() {
-    hGraph test = kGraph(20);
-    test.calcEulerChar();
-    std::cout << test ;
-
+    hGraph test = randomGraph(100);
+    std::ofstream output;
+    output.open("test.csv");
+    output << "100\n";
+    output << test;
+    output.close();
     
     
 }
 
 
+/*
+ 
+ 
+ Calculating shortest cycle at node k
+ 
+ Find all neighbors of k and keep track of all walks. Obviously none are cycles
+ Make another step from each of the resultant paths
+ 
+ Check if any of the last items in the three-walks is equal to the last node in the two-walk. If so, the shortest cycle is 3.
+ 
+ Then go to four-walks. If any of the last items in the 4-walks is the last item in one of the two-walks, the shortest cycle is four.
+ If not, check all four walks against eachother. If two cycles have the same end points, and are not identical, then the shortest cycle is four.
+ 
+ At each iteration, must check all previous walks and see if two walks terminate on the same node. However if they contain a shared internal node, then it is not a loop.  
+ 
+ 
+ 
+ 
+ 
+ */
